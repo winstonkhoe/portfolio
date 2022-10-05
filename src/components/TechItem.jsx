ca3fbc547b5d3e40f260c6d1e0445d6d96579567
winstonkhoe <winstonkhoe@yahoo.com>
2022-10-06 00:37:02 +0700

    Tried to make repositories preview

--- src/components/TechItem.jsx
+++ src/components/TechItem.jsx
@@ -1,16 +1,43 @@
-const TechItem = ({name, image, func}) => {
-    const mouseEnter = () => {
-        func(name);
-    }
+import { useState } from "react";
 
-    const mouseLeave = () => {
-        func("");
-    }
-    return (
-        <a href="#" aria-label={name} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-            <img src={"images/" + image} alt=""/>
-        </a>
-    )
-}
+const TechItem = ({ name, image, func }) => {
+  const [onHover, setOnHover] = useState(false);
+  const mouseEnter = () => {
+    func(name);
+    setOnHover(true);
+  };
+
+  const mouseLeave = () => {
+    func("");
+    setOnHover(false);
+  };
+  return (
+    <div style={{ position: "relative", transformStyle: "preserve-3d" }}>
+      {onHover && (
+        <div
+          style={{
+            width: "300px",
+            height: "300px",
+            position: "absolute",
+            right: "100%",
+            backgroundColor: "#FFF",
+            transform: "rotateY(40deg)",
+            transformOrigin: "left",
+            zIndex: "999",
+          }}
+        ></div>
+      )}
+      <a
+        href="#"
+        aria-label={name}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        style={{ position: "absolute", zIndex: "1" }}
+      >
+        <img src={"images/" + image} alt="" />
+      </a>
+    </div>
+  );
+};
 
 export default TechItem;
